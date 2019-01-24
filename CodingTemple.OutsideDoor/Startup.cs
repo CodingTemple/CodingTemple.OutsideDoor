96882b1ae218d5c14846735f5dc5c1444401b1a3
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-01-24 09:35:42 -0600

    Change OutsideDoor to calculate current time inside of async method instead of Configure to prevent caching bug.

--- CodingTemple.OutsideDoor/Startup.cs
+++ CodingTemple.OutsideDoor/Startup.cs
@@ -30,36 +30,33 @@ namespace CodingTemple.OutsideDoor
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            DayOfWeek[] Weekdays = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
-            TimeZoneInfo centralZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
-            DateTime currentCentralTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, centralZone);
-
-            VoiceResponse response = new VoiceResponse();
-
+            DayOfWeek[] days =
+                Configuration.GetValue<string>("Days", "1, 2, 3, 4, 5, 6").Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => (DayOfWeek)int.Parse(x)).ToArray();
+            //new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+          
             double startHour = Configuration.GetValue<double>("StartHour", 9);
             double endHour = Configuration.GetValue<double>("EndHour", 18);
             
-
             string[] phoneNumbers = Configuration.GetValue<string>("PhoneNumbers", "").Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
             
-
-            if ((Weekdays.Contains(currentCentralTime.DayOfWeek)) && (currentCentralTime > currentCentralTime.Date.AddHours(startHour)) && (currentCentralTime < currentCentralTime.Date.AddHours(endHour)))
-            {
-                response.Play(null, null, "ww9ww9ww9");
-                response.Hangup();
-            }
-            else if (phoneNumbers.Any())
-            {
-                phoneNumbers.ToList().ForEach(x => { response.Dial(x); });
-            }
-            else
-            {
-                response.Say("Access Denied");
-            }
-            //return new TwiMLResult(response);
-
             app.Run(async (context) =>
             {
+                VoiceResponse response = new VoiceResponse();
+                TimeZoneInfo centralZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+                DateTime currentCentralTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, centralZone);
+                if ((days.Contains(currentCentralTime.DayOfWeek)) && (currentCentralTime > currentCentralTime.Date.AddHours(startHour)) && (currentCentralTime < currentCentralTime.Date.AddHours(endHour)))
+                {
+                    response.Play(null, null, "ww9ww9ww9");
+                    response.Hangup();
+                }
+                else if (phoneNumbers.Any())
+                {
+                    phoneNumbers.ToList().ForEach(x => { response.Dial(x); });
+                }
+                else
+                {
+                    response.Say("Access Denied");
+                }
                 context.Response.ContentType = "text/xml";
                 await context.Response.WriteAsync(response.ToString());
             });
